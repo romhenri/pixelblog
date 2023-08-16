8e12aee6b607aee6415b53d10e05a60865555576
Rômulo Henri <romulohenri000@gmail.com>
2023-08-15 21:32:06 -0300

    chore: Singup
    improve: Singup layout

--- src/pages/SingupPage.jsx
+++ src/pages/SingupPage.jsx
@@ -1,26 +1,78 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import ButtonsLine from '../components/ButtonsLine'
 
 const SingupPage = () => {
+  const [displayName, setDisplayName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setError("")
+
+    const user = {
+      displayName,
+      email,
+      password
+    }
+
+    if (password !== confirmPassword) {
+      setError("As senhas precisam ser iguais!")
+      return
+    }
+
+    console.log(displayName, email, password, confirmPassword);
+    console.log(user);
+  } 
+
   return (
     <main>
-
       <section className='simple'>
         <h2>Cadastre-se</h2>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
 
-          <Input label="Nome:" placeholder={"Nome de exbição"} required/>
+          <Input
+            label="Nome:"
+            placeholder={"Nome de exbição"}
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          required/>
 
-          <Input label="Email:" type="email" placeholder={"Email para registro"} required/>
+          <Input
+            label="Email:"
+            type="email"
+            placeholder={"Email para registro"}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required/>
 
-          <Input label="Senha:" type="password" placeholder={"Insira sua senha"} required/>
+          <Input
+            label="Senha:"
+            type="password"
+            placeholder={"Insira sua senha"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required/>
 
-          <Input label="Confirme sua senha:" type="password" placeholder={"Insira sua senha novamente"} required/>
+          <Input
+            label="Confirme sua senha:"
+            type="password"
+            placeholder={"Insira sua senha novamente"}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required/>
         
           <label>Use uma senha Forte!</label>
+
+          <div>
+            {error && <p className='error'>{error}</p>}
+          </div>
 
           <ButtonsLine>
             <Button text={"Continuar sem Conta"} bg="transparent"
